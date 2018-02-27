ff9012423f966ab2c37c7cf5b75e6893c270e591
artem.alagizov <artem.alagizov@gmail.com>
2018-02-27 14:51:09 +0100

    Extended hello-world a wee bit

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,4 +1,18 @@
-module Lib ( someFunc ) where
+module Lib ( printResponse ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "Hello world"
+import System.Random (randomRIO)
+
+pickRandom :: [a] -> IO a
+pickRandom xs = fmap (xs !!) $ randomRIO (0, length xs - 1)
+
+printResponse :: IO ()
+printResponse = do
+    x <- pickRandom [
+        "Serious trouble will bypass you, innit?",
+        "The greatest risk is not taking one, bruv.",
+        "Keep your eye out for someone special. An assassin, a robot-prostitute or, perhaps, a poisonous snake.",
+        "A very attractive person has a message for you. It says - 'don't ever bother me again, ya fucking cunt'.",
+        "If winter comes, can spring be far behind? It, surely, can.",
+        "Your ability for accomplishment will follow with success. Maybe."
+        ]
+    print x
