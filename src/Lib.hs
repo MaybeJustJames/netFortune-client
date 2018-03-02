f08a3547c2e486dc67fe13798dca83f3e7042dc5
artem.alagizov <artem.alagizov@gmail.com>
2018-03-02 13:38:04 +0100

    Modified code to return prediction to the client

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,18 +1,18 @@
-module Lib ( printResponse ) where
+module Lib ( getFortunePrediction ) where
 
 import System.Random (randomRIO)
 
 pickRandom :: [a] -> IO a
 pickRandom xs = fmap (xs !!) $ randomRIO (0, length xs - 1)
 
-printResponse :: IO ()
-printResponse = do
+getFortunePrediction :: IO String
+getFortunePrediction = do
     x <- pickRandom [
         "Serious trouble will bypass you, innit?",
         "The greatest risk is not taking one, bruv.",
-        "Keep your eye out for someone special. An assassin, a robot-prostitute or, perhaps, a poisonous snake.",
+        "Keep your eye out for someone special. An assassin, a robot-prostitute from the future or, perhaps, a poisonous snake.",
         "A very attractive person has a message for you. It says - 'don't ever bother me again, ya fucking cunt'.",
         "If winter comes, can spring be far behind? It, surely, can.",
         "Your ability for accomplishment will follow with success. Maybe."
         ]
-    print x
+    return x
