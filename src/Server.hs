bcfbbb40c1e15be3e42e74b3e4d6c67f508a6b3e
artem.alagizov <artem.alagizov@gmail.com>
2018-03-02 14:07:46 +0100

    Added routes (snaplet-based)

--- src/Server.hs
+++ src/Server.hs
@@ -2,17 +2,32 @@
 
 module Server ( serve ) where
 
+import Data.ByteString
 import Data.Text
 import Control.Monad.IO.Class -- liftIO lives here
 import Snap
 import Lib
 
-app :: Snap ()
-app = do
-    x <- liftIO $ getFortunePrediction -- liftIO unwraps IO monad
-    writeText . pack $ x -- writeText expects Text, x is a String, pack converts String to Text
+-- | Application database pool, state, etc. can be added here
+data App = App {}
+
+-- | Application routes
+routes :: [(ByteString, Handler App App ())]
+routes = [("/fortune", method GET $ predictFortune)]
+
+-- | Fortune route handler
+predictFortune :: Handler App App ()
+predictFortune = do
+     x <- liftIO $ getFortunePrediction -- liftIO unwraps IO monad
+     writeText . Data.Text.pack $ x -- writeText expects Text, x is a String, pack converts String to Text
+
+-- | Initialisation
+appInit :: SnapletInit App App
+appInit = makeSnaplet "microservice" "Fortune microservice" Nothing $ do
+    addRoutes routes
+    return $ App
 
 serve :: IO ()
 serve = do
-    -- Serve the app, accessible at http://localhost:8080
-    httpServe (setPort 8080 defaultConfig) app
+    -- Serve the app
+    serveSnaplet (setPort 8080 defaultConfig) appInit
