310352c2fcba5220414ea0d7eb81a98238d712e7
ArtemAlagizov <alagizov@gmail.com>
2018-03-18 15:01:44 +0100

    Minor change to indentation

--- test/TestUtils.hs
+++ test/TestUtils.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-module TestUtils (shouldHaveResponse) where
+module TestUtils ( shouldHaveResponse ) where
 
 import qualified Data.Text               as T
 import           Test.Hspec.Core.Spec
@@ -9,7 +9,7 @@ import qualified Test.Hspec.Snap         as T
 -- | Asserts that the response (which should be Html) contains any text.
 shouldHaveResponse :: T.TestResponse -> T.SnapHspecM b ()
 shouldHaveResponse (T.Html _ body) =
-  if T.length body > 0
-  then T.setResult Success
-  else T.setResult (Failure Nothing $ Reason $ T.unpack $ T.concat ["Body contains: ", body])
+    if T.length body > 0
+    then T.setResult Success
+    else T.setResult (Failure Nothing $ Reason $ T.unpack $ T.concat ["Body contains: ", body])
 shouldHaveResponse x = T.setResult (Failure Nothing $ Reason (show x))
