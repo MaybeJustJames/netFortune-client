926fea250fe12888acfde96db6a8995721505434
ArtemAlagizov <alagizov@gmail.com>
2018-03-18 10:39:50 +0100

    Extended prediction base, modified server tests.

--- test/Spec.hs
+++ test/Spec.hs
@@ -2,6 +2,7 @@
 
 import           Server
 import           Snap
+import           TestUtils
 import           Test.Hspec
 import qualified Test.Hspec.Snap as T
 
@@ -12,8 +13,10 @@ tests = do
         describe "Application" $ do
             it "Has /fortune route" $ do
                 T.get "/fortune" >>= T.should200
+            it "Has no /non-existing route" $ do
+                T.get "/non-existing" >>= T.should404
             it "Has response" $ do
-                T.get "/fortune" >>= T.shouldHaveText "Behold!"
+                T.get "/fortune" >>= shouldHaveResponse
 
 main :: IO()
 main = do
